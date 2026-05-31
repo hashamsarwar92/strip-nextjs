376e9bb264d12b1a31403b528fe5f9c72704ac82
Hasham Sarwar <hsarwar4227@gmail.com>
2026-05-31 12:45:08 +0100

    fix searchparams

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,14 @@ import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 
 export default function Home() {
-  const searchParams = useSearchParams();
-  const status = searchParams.get("status");
-  const message =
-    status === "success"
-      ? "Payment successful."
-      : status === "failed"
-      ? "Payment failed."
-      : null;
+  // const searchParams = useSearchParams();
+  // const status = searchParams.get("status");
+  // const message =
+  //   status === "success"
+  //     ? "Payment successful."
+  //     : status === "failed"
+  //     ? "Payment failed."
+  //     : null;
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
