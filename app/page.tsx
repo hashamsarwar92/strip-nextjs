e2352113d5dd9e0b78ce63909b1d4af595d3e91c
Hasham Sarwar <hsarwar4227@gmail.com>
2026-05-31 12:46:43 +0100

    fix message error

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        {message ? (
+        {/* {message ? (
           <p
             className={`rounded-full px-4 py-2 text-sm font-medium ${
               status === "success"
@@ -26,7 +26,7 @@ export default function Home() {
           >
             {message}
           </p>
-        ) : null}
+        ) : null} */}
         <Image
           className="dark:invert"
           src="/next.svg"
